b89f29b175b0a282f7024fb4d9236f51bd2c2774
Samuel Riddle <samuel.riddle1@gmail.com>
2021-11-25 22:04:59 -0700

    Adding LP Token pairs

--- src/constants/pit.ts
+++ src/constants/pit.ts
@@ -10,24 +10,88 @@ export const PIT_POOLS: {
   [ChainId.HARMONY_MAINNET]: [
     {
       pid: 0,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/WONE')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX POOL')
     },
     {
       pid: 1,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/1USDC')
     },
     {
       pid: 2,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1ETH')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/WONE')
     },
     {
       pid: 3,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'BUSD/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/UST')
     },
     {
       pid: 4,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'BUSD/bscBUSD')
-    }
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/JEWEL')
+    },
+    {
+      pid: 5,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/MIS')
+    },
+    {
+      pid: 6,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/RAVAX')
+    },
+    {
+      pid: 7,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'FOX/RVRS')
+    },
+    {
+      pid: 8,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'UST/MIS')
+    },
+    {
+      pid: 9,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'UST/WONE')
+    },
+    {
+      pid: 10,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/MIS')
+    },
+    {
+      pid: 11,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'TRANQ/MIS')
+    },
+    {
+      pid: 12,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WBTC/MIS')
+    },
+    {
+      pid: 13,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'JEWEL/MIS')
+    },
+    {
+      pid: 14,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'XYA/MIS')
+    },
+    {
+      pid: 15,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'UST/1USDC')
+    },
+    {
+      pid: 16,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'LUNA/MIS')
+    },
+    {
+      pid: 17,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'RFTM/MIS')
+    },
+    {
+      pid: 18,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'MIS/RVRS')
+    },
+    {
+      pid: 19,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1USDC/WONE')
+    },
+    {
+      pid: 20,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'UST/RVRS')
+    },
   ],
   [ChainId.HARMONY_TESTNET]: [
     {
