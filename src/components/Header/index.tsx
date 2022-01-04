8630928a491a439987a635187746881c04f2d671
Riddle <samuel.riddle1@gmail.com>
2022-01-03 20:10:25 -0700

    Changed header to include fox price and balance

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
-// import { TokenAmount } from '@foxswap/sdk'
+import { TokenAmount } from '@foxswap/sdk'
 import React, { useState } from 'react'
 import { Text } from 'rebass'
+import { Moon, Sun } from 'react-feather'
 import { NavLink } from 'react-router-dom'
 import { darken } from 'polished'
 import { useTranslation } from 'react-i18next'
@@ -10,12 +11,10 @@ import FoxLogo from 'assets/svg/foxswap/foxswaplogo-iconwhite.svg'
 import BlackFoxLogo from 'assets/svg/foxswap/foxswaplogo-iconblack.svg'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
-import { useETHBalances } from '../../state/wallet/hooks'
+import { useTokenBalance } from '../../state/wallet/hooks'
+import useGovernanceToken from '../../hooks/useGovernanceToken'
 import { CardNoise } from '../earn/styled'
-// import { CountUp } from 'use-count-up'
 import { ExternalLink, TYPE } from '../../theme'
-
-import { Moon, Sun } from 'react-feather'
 import Menu from '../Menu'
 
 import Row, { RowFixed } from '../Row'
@@ -27,17 +26,16 @@ import { useUserHasSubmittedClaim } from '../../state/transactions/hooks'
 import { Dots } from '../swap/styleds'
 import Modal from '../Modal'
 import GovTokenBalanceContent from './GovTokenBalanceContent'
-// import usePrevious from '../../hooks/usePrevious'
-import { BASE_CURRENCY } from '../../connectors'
+
+import { GOVERNANCE_TOKEN_INTERFACE } from '../../constants/abis/governanceToken'
+// import { BASE_CURRENCY } from '../../connectors'
 import { PIT_SETTINGS } from '../../constants'
-import useGovernanceToken from '../../hooks/useGovernanceToken'
 
 const HeaderFrame = styled.div`
   display: grid;
   grid-template-columns: 1fr 120px;
   align-items: center;
   justify-content: space-between;
-  align-items: center;
   flex-direction: row;
   width: 100%;
   top: 0;
@@ -242,10 +240,6 @@ export const StyledMenuButton = styled.button`
   width: 100%;
   height: 100%;
   border: none;
-  background-color: transparent;
-  margin: 0;
-  padding: 0;
-  height: 35px;
   background-color: ${({ theme }) => theme.bg3};
   margin-left: 8px;
   padding: 0.15rem 0.5rem;
@@ -275,7 +269,12 @@ export default function Header() {
   const [showUniBalanceModal, setShowUniBalanceModal] = useState(false)
 
   const govToken = useGovernanceToken()
-  const userEthBalance = useETHBalances(account ? [account] : [])?.[account ?? '']
+  const userFoxBalance: TokenAmount | undefined = useTokenBalance(
+    account ?? undefined,
+    govToken,
+    'balanceOf',
+    GOVERNANCE_TOKEN_INTERFACE
+  )
   const pitSettings = chainId ? PIT_SETTINGS[chainId] : undefined
   const toggleClaimModal = useToggleSelfClaimModal()
   const availableClaim: boolean = useUserHasAvailableClaim(account)
@@ -313,13 +312,13 @@ export default function Header() {
           >
             {t('pool')}
           </StyledNavLink>
+          <StyledNavLink id={`pit-nav-link`} to={`${pitSettings?.path}`}>
+            {t('Stake')}
+          </StyledNavLink>
           <StyledNavLink id={`bond-nav-link`} to={'/bond'}>
             {t('Bond')}
           </StyledNavLink>
           <StyledRedirectLink href={`https://app.farmersonly.fi/vaults`}>{t('Vault')}</StyledRedirectLink>
-          <StyledNavLink id={`pit-nav-link`} to={`${pitSettings?.path}`}>
-            {pitSettings?.name}
-          </StyledNavLink>
         </HeaderLinks>
       </HeaderRow>
       <HeaderControls>
@@ -339,9 +338,9 @@ export default function Header() {
             </UNIWrapper>
           )}
           <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
-            {account && userEthBalance ? (
+            {account && userFoxBalance ? (
               <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
-                {userEthBalance?.toSignificant(4)} {BASE_CURRENCY.symbol}
+                {userFoxBalance?.toSignificant(4)} {govToken?.symbol}
               </BalanceText>
             ) : null}
             <Web3Status />
